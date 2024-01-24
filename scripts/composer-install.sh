0fd0107b665b2e850457d9db87a8e38d341befd7
Majid <mj_azani@hotmail.com>
2024-01-24 23:51:45 +1030

    composer install script added

---
+++ scripts/composer-install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Exit on any command failure.
+set -e
+
+# Download the Composer installer.
+EXPECTED_SIGNATURE="$(curl -sS https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+# Verify the installer signature.
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+# Run the installer.
+php composer-setup.php --quiet --install-dir=/usr/local/bin --filename=composer
+RESULT=$?
+rm composer-setup.php
+
+# Check outcome of installation.
+if [ $RESULT -eq 0 ]; then
+    echo "Composer successfully installed!"
+else
+    echo "Composer installation failed."
+    exit 1
+fi
+
+# Check Composer version.
+composer --version
