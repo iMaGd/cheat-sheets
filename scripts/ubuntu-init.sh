8ff9770b1e9b29867b16024855488332935a9d12
Majid <mj_azani@hotmail.com>
2024-01-26 16:50:44 +1030

    software-properties-common added

--- scripts/ubuntu-init.sh
+++ scripts/ubuntu-init.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Exit on error
+set -e
+
+# Updates the package list and installs required packages.
 sudo apt update && sudo apt upgrade -y
+
+# enables easier management of third-party software sources and is important when you want to install software from sources that are not available in the default Ubuntu repositories
+sudo apt install -y software-properties-common
 
 sudo apt install -y \
   unzip zip nano openssh-server \
