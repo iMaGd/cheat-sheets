0e02dfcb0d31bb6c23e47cabaf232736e430d440
Majid <mj_azani@hotmail.com>
2024-01-27 22:15:24 +1030

    sudo added

--- scripts/wp-add.sh
+++ scripts/wp-add.sh
@@ -23,8 +23,8 @@ read -p "Enter WP installation path (/var/www/html): " wp_path
 
 # Download WordPress core
 if prompt_yes_no "Do you want to download WordPress core in current directory?"; then
-    wp core download --path="$wp_path"
-    echo "WordPress core downloaded."
+	sudo wp core download --path="$wp_path"
+	echo "WordPress core downloaded."
 else
     echo "WordPress core download skipped."
 fi
@@ -43,13 +43,13 @@ if prompt_yes_no "Setup WordPress configuration?"; then
 	dbprefix=${dbprefix:-wp_}
 	read -p "Do you want to enable WP_DEBUG? (y/n) " wp_debug
 
-	wp core config --dbname="$dbname" --dbuser="$dbuser" --dbpass="$dbpass" --dbhost="$dbhost" --dbprefix="$dbprefix" --extra-php --path="$wp_path" <<PHP
+	sudo wp core config --dbname="$dbname" --dbuser="$dbuser" --dbpass="$dbpass" --dbhost="$dbhost" --dbprefix="$dbprefix" --extra-php --path="$wp_path" <<PHP
 define( 'WP_DEBUG', $( [[ "$wp_debug" == "y" ]] && echo 'true' || echo 'false' ) );
 define( 'WP_DEBUG_LOG', $( [[ "$wp_debug" == "y" ]] && echo 'true' || echo 'false' ) );
 PHP
 
 	# Create the database
-	wp db create --path="$wp_path"
+	sudo wp db create --path="$wp_path"
 
 fi
 
@@ -62,7 +62,7 @@ if prompt_yes_no "Do you want to install a WordPress Multisite?"; then
 	# Generate a strong password
 	admin_pass=$(openssl rand -base64 12)
 
-    wp core multisite-install --url="$site_url" --base=/ --title="Website" --admin_user="$admin_user" --admin_password="$admin_pass" --admin_email="admin@domain.com" --path="$wp_path" --skip-email
+	sudo wp core multisite-install --url="$site_url" --base=/ --title="Website" --admin_user="$admin_user" --admin_password="$admin_pass" --admin_email="admin@domain.com" --path="$wp_path" --skip-email
 
 	# Output the username and password
 	echo "User '$admin_user' created with the password '$admin_pass'"
@@ -74,20 +74,20 @@ elif prompt_yes_no "or install a WordPress single site?"; then
 	# Generate a strong password
 	admin_pass=$(openssl rand -base64 12)
 
-    wp core install --url="$site_url" --title="Website" --admin_user="$admin_user" --admin_password="$admin_pass" --admin_email="admin@domain.com" --path="$wp_path"
+	sudo wp core install --url="$site_url" --title="Website" --admin_user="$admin_user" --admin_password="$admin_pass" --admin_email="admin@domain.com" --path="$wp_path"
 fi
 
 if prompt_yes_no "Install starter theme and plugins?"; then
-	wp plugin deactivate hello-dolly --path="$wp_path"
-	wp plugin delete hello-dolly --path="$wp_path"
+	sudo wp plugin deactivate hello-dolly --path="$wp_path"
+	sudo wp plugin delete hello-dolly --path="$wp_path"
 
-	wp theme install phlox --activate --path="$wp_path"
-	wp plugin install auxin-elements really-simple-ssl autoptimize duplicate-page --activate --path="$wp_path"
+	sudo wp theme install phlox --activate --path="$wp_path"
+	sudo wp plugin install auxin-elements really-simple-ssl autoptimize duplicate-page --activate --path="$wp_path"
 fi
 
 # Enable SSL (search-replace)
 if prompt_yes_no "Replace http with https in database?"; then
-    wp search-replace "http://" "https://"
+	sudo wp search-replace "http://" "https://"
     echo "SSL enabled."
 else
     echo "SSL enabling skipped."
