cbb77f94c41e064f0a2e2b82686f1b5b7c4e4844
Majid <mj_azani@hotmail.com>
2024-01-27 17:03:03 +1030

    simplifing the process

--- scripts/mysql-adduser.sh
+++ scripts/mysql-adduser.sh
@@ -3,18 +3,23 @@
 set -e
 
 # Prompt for database root user credentials
-read -p "Enter MySQL/MariaDB root user: " root_user
+read -p "Enter MySQL/MariaDB root user [Default: root]: " root_user
+root_user=${root_user:-root}
 read -s -p "Enter MySQL/MariaDB root password: " root_pass
 echo ""
 
 # Prompt for new database username and password
-read -p "Enter new database username: " db_user
-read -s -p "Enter new database user password: " db_pass
+read -p "Enter new database user: " db_user
+read -s -p "Enter new database user password (leave blank to auto generate a strong password): " db_pass
 echo ""
+
+# Generate a strong password if it is empty
+if [[ -z "${db_pass// }" ]]; then
+    db_pass=$(openssl rand -base64 12)
+fi
 
 # Prompt for database where the privileges should be set
 read -p "Enter database name (leave blank for all databases): " db_name
-
 
 # Connect to MySQL/MariaDB server and create new user
 mysql --user="$root_user" --password="$root_pass" --execute="
@@ -32,5 +37,8 @@ else
 fi
 
 mysql --user="$root_user" --password="$root_pass" --execute="FLUSH PRIVILEGES;"
+
+# Output the username and password
+echo "New database user '$db_user' created with the password '$db_pass'"
 
 echo "Privileges have been assigned."
