cd08de15acd30ea9313bd9aca4adda413be7eac2
Majid <mj_azani@hotmail.com>
2024-01-27 19:15:36 +1030

    prevent overwriting existing conf file

--- scripts/apache-add-site.sh
+++ scripts/apache-add-site.sh
@@ -3,10 +3,6 @@
 # Exit on error
 set -e
 
-# Enable mod_proxy_fcgi and setenvif modul
-sudo a2enmod rewrite proxy_fcgi setenvif
-sudo service apache2 restart
-
 # Ask for domain names
 read -p "Enter the domain name for new site (e.g., www.example.com): " new_domain
 
@@ -21,9 +17,39 @@ php_version=${php_version:-8.1}
 # Construct the PHP-FPM socket path
 php_fpm_socket="/var/run/php/php${php_version}-fpm.sock"
 
-# Setup WordPress VirtualHost
-echo "Configuring VirtualHost for new site..."
-sudo tee /etc/apache2/sites-available/"$new_domain".conf > /dev/null << EOF
+# Site configuration file
+vhost_conf_file="/etc/apache2/sites-available/${new_domain}.conf"
+
+# Function to prompt for y/n question
+prompt_yes_no() {
+    while true; do
+        # Ask the user
+        read -rp "$1 [Y/n]: " answer
+
+        # Default to Yes if no answer is given.
+        if [[ -z $answer ]]; then
+            answer="N"
+        fi
+
+        # Check the answer
+        case "$answer" in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+# Function to add a VirtualHost
+add_new_virtualhost() {
+
+	# Enable mod_proxy_fcgi and setenvif modul
+	sudo a2enmod rewrite proxy_fcgi setenvif
+	sudo service apache2 restart
+
+	# Setup WordPress VirtualHost
+	echo "Configuring VirtualHost for new site..."
+	sudo tee "$vhost_conf_file" > /dev/null << EOF
 <VirtualHost *:80>
 	ServerName $new_domain
 	# ServerAlias www.${new_domain}
@@ -31,11 +57,11 @@ sudo tee /etc/apache2/sites-available/"$new_domain".conf > /dev/null << EOF
 	DocumentRoot $site_path
 
 	<Directory "$site_path">
-        # Don't show directory index
-        Options -Indexes +FollowSymLinks +MultiViews
-        # Allow .htaccess files
+		# Don't show directory index
+		Options -Indexes +FollowSymLinks +MultiViews
+		# Allow .htaccess files
 		AllowOverride All
-        # Allow web access to this directory
+		# Allow web access to this directory
 		Require all granted
 	</Directory>
 	<FilesMatch \.php$>
@@ -44,22 +70,39 @@ sudo tee /etc/apache2/sites-available/"$new_domain".conf > /dev/null << EOF
 	</FilesMatch>
 
 	ErrorLog \${APACHE_LOG_DIR}/${new_domain}.error.log
-    CustomLog \${APACHE_LOG_DIR}/${new_domain}.access.log combined
+	CustomLog \${APACHE_LOG_DIR}/${new_domain}.access.log combined
 </VirtualHost>
 EOF
 
-# Enable new site
-sudo a2ensite "$new_domain"
+	# Enable new site
+	sudo a2ensite "$new_domain"
+}
+
+# Check if the site configuration already exists
+if [ -f "$vhost_conf_file" ]; then
+	echo "A configuration file for this site already exists: ${vhost_conf_file}"
+
+	if prompt_yes_no "Do you want to overwrite existing VirtualHost?!"; then
+		add_new_virtualhost
+	fi
+else
+	add_new_virtualhost
+fi
+
+sudo service apache2 restart
 
 # Disable the default Apache site
-echo "Disabling default Apache VirtualHost..."
-sudo a2dissite 000-default.conf
-
-sudo service apache2 reload
+if prompt_yes_no "Disable default Apache VirtualHost?"; then
+	echo "Disabling default Apache VirtualHost..."
+	sudo a2dissite 000-default.conf
+	sudo service apache2 reload
+fi
 
 # Create the info.php file with phpinfo()
-sudo mkdir -p $site_path
-sudo bash -c "echo '<?php phpinfo(); ?>' > $site_path/info.php"
+if prompt_yes_no "Do you want to add a info.php file with phpinfo() in the new site directory?"; then
+	sudo mkdir -p $site_path
+	sudo bash -c "echo '<?php phpinfo(); ?>' > $site_path/info.php"
+fi
 
 # Set appropriate permissions for the info.php file
 sudo chown www-data:www-data "${site_path}/info.php"
@@ -69,5 +112,5 @@ sudo chmod 644 "${site_path}/info.php"
 echo "Reloading Apache..."
 sudo service apache2 reload
 
-echo "New virtual host file added at /etc/apache2/sites-available/${new_domain}.conf."
+echo "New virtualHost file added at ${vhost_conf_file}."
 echo "New site is configured successfully."
