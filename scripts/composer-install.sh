58dfc4fcc1a15d38c85210352d8d48a5e4501f58
Majid <mj_azani@hotmail.com>
2024-01-30 21:20:49 +1030

    fixing the permission issue

--- scripts/composer-install.sh
+++ scripts/composer-install.sh
@@ -5,21 +5,21 @@ set -e
 
 # Download the Composer installer.
 EXPECTED_SIGNATURE="$(curl -sS https://composer.github.io/installer.sig)"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+php -r "copy('https://getcomposer.org/installer', '/tmp/composer-setup.php');"
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', '/tmp/composer-setup.php');")"
 
 # Verify the installer signature.
 if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
 then
     >&2 echo 'ERROR: Invalid installer signature'
-    sudo rm composer-setup.php
+    sudo rm /tmp/composer-setup.php
     exit 1
 fi
 
 # Run the installer.
-sudo php composer-setup.php --quiet --install-dir=/usr/local/bin --filename=composer
+sudo php /tmp/composer-setup.php --quiet --install-dir=/usr/local/bin --filename=composer
 RESULT=$?
-sudo rm composer-setup.php
+sudo rm /tmp/composer-setup.php
 
 # Check outcome of installation.
 if [ $RESULT -eq 0 ]; then
