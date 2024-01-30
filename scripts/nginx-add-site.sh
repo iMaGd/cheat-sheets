c4a9499a72ee29901bb5c912cc4bfa98b530046b
Majid <mj_azani@hotmail.com>
2024-01-30 21:00:06 +1030

    site configuration added to nginx script

--- scripts/nginx-add-site.sh
+++ scripts/nginx-add-site.sh
@@ -12,20 +12,19 @@ read -p "Enter the DocumentRoot for your new site [default: /var/www/html]: " si
 site_path=${site_path:-/var/www/html}
 
 # Ask for the PHP-FPM version to use
-read -p "Enter the PHP-FPM version to use with Apache [default: 8.1]: " php_version
+read -p "Enter the PHP-FPM version to use with Nginx [default: 8.1]: " php_version
 php_version=${php_version:-8.1}
 
 # Construct the PHP-FPM socket path
 php_fpm_socket="/var/run/php/php${php_version}-fpm.sock"
+vhost_conf_file="/etc/nginx/sites-available/${site_name}"
 
-# Setup WordPress VirtualHost
-echo "Configuring VirtualHost for new site..."
 
-sudo service nginx restart
+add_new_virtualhost() {
+    echo "Configuring VirtualHost for new site..."
+    sudo mkdir -p $site_path
 
-conf_file="/etc/nginx/sites-available/${site_name}"
-
-sudo tee "$conf_file" > /dev/null << EOF
+    sudo tee "$vhost_conf_file" > /dev/null << EOF
 server {
     listen 80;
     server_name $new_domain;
@@ -56,15 +55,114 @@ server {
 }
 EOF
 
-# Check config file
-echo "--------------------"
-echo -e "New conf file added at ${conf_file}.\n"
-sudo cat ${conf_file}
-echo -e "--------------------\n"
+    # Enable the configuration by creating symlinks
+    sudo ln -s /etc/nginx/sites-available/$site_name /etc/nginx/sites-enabled/
+
+    # Check config file
+    echo "--------------------"
+    echo -e "New conf file added at ${vhost_conf_file}.\n"
+    sudo cat ${vhost_conf_file}
+    echo -e "--------------------\n"
+}
+
+# Check if the site configuration already exists
+if [ -f "$vhost_conf_file" ]; then
+	echo "A configuration file for this site already exists: ${vhost_conf_file}"
+
+	if prompt_yes_no "Do you want to overwrite existing VirtualHost?!"; then
+		add_new_virtualhost
+	fi
+else
+	add_new_virtualhost
+fi
+
+# Reload to apply changes
+sudo service nginx restart
+
+# Disable the default Nginx site
+if prompt_yes_no "Disable default Nginx site?"; then
+	# Disable the default site by removing the symbolic link
+    sudo rm /etc/nginx/sites-enabled/default
+	sudo service nginx reload
+fi
+
+# Create the info.php file with phpinfo()
+if prompt_yes_no "Do you want to add a info.php file with phpinfo() in the new site directory?"; then
+	sudo mkdir -p $site_path
+	sudo bash -c "echo '<?php phpinfo(); ?>' > $site_path/info.php"
+	sudo chmod 644 "${site_path}/info.php"
+fi
+
+# Check Nginx configuration for syntax errors
+sudo nginx -t
+
+# Reload to apply changes
+sudo systemctl reload nginx
+
+# Create the info.php file with phpinfo()
+if prompt_yes_no "Do you want to run this site with 'www-data' user?"; then
+	# Set appropriate permissions
+	sudo chown -R www-data:www-data "${site_path}"
 
 
-# Enable the configuration by creating symlinks
-sudo ln -s /etc/nginx/sites-available/$site_name /etc/nginx/sites-enabled/
+elif prompt_yes_no "Do you want to run this site with another user instead of 'www-data' user?"; then
+
+	read -p "Enter the username: " site_user
+
+	pool_dir="/etc/php/${php_version}/fpm/pool.d"
+	pool_file="${pool_dir}/${site_user}.conf"
+
+	# Check if the PHP-FPM pool already exists
+	if [ -f "${pool_file}" ]; then
+		echo "PHP-FPM pool configuration for user '${site_user}' already exists: ${pool_file}"
+		exit 1
+	fi
+
+	# Create PHP-FPM pool configuration for the user
+	echo "Creating PHP-FPM pool for '${site_user}'..."
+	sudo bash -c "cat > ${pool_file} << EOF
+[${site_user}]
+user = ${site_user}
+group = ${site_user}
+listen = /var/run/php/php${php_version}-fpm-${site_user}.sock
+listen.owner = ${site_user}
+listen.group = ${site_user}
+pm = dynamic
+pm.max_children = 35
+pm.start_servers = 8
+pm.min_spare_servers = 8
+pm.max_spare_servers = 26
+chdir = /
+security.limit_extensions = .php
+php_admin_value[disable_functions] = exec,passthru,shell_exec,system
+php_admin_flag[allow_url_fopen] = on
+EOF"
+
+
+	# Check config file
+	echo "--------------------"
+	echo "New PHP-FPm pool file added at ${pool_file}."
+	sudo cat ${pool_file}
+	echo -e "--------------------\n"
+
+	# Restart to apply the configuration
+	echo "Restarting PHP-FPM..."
+	sudo service php${php_version}-fpm restart
+
+	# Update Nginx configuration to use the user's PHP-FPM pool
+	echo "Updating Nginx configuration for '${site_user}'..."
+	sudo sed -i "s|unix:/run/php/php${php_version}-fpm.sock|unix:/var/run/php/php${php_version}-fpm-${site_user}.sock|g" "${vhost_conf_file}"
+	sudo sed -i "s|unix:/var/run/php/php${php_version}-fpm.sock|unix:/var/run/php/php${php_version}-fpm-${site_user}.sock|g" "${vhost_conf_file}"
+
+	# Set appropriate permissions
+	sudo chown -R ${site_user}:${site_user} ${site_path}
+
+	# Since Apache/Nginx runs under the `www-data` user and requires only read access
+	# simply add `www-data` to the '${site_user}' group.
+	# This will enable `www-data` to read files owned by the '${site_user}' user
+	sudo usermod -aG ${site_user} www-data
+
+fi
 
 # Check Nginx configuration for syntax errors
 sudo nginx -t
