74542446dc9fdc170827445a417c4c7db2e8773a
Majid <mj_azani@hotmail.com>
2024-02-04 20:33:07 +1030

    install apache benchmark as well

--- scripts/nginx-install.sh
+++ scripts/nginx-install.sh
@@ -7,5 +7,8 @@ set -e
 echo "Installing Nginx..."
 sudo apt install -y nginx
 
+# Install web server benchmark tool
+sudo apt install -y apache2-utils
+
 # Enable Nginx to run on system start
 sudo systemctl enable nginx
