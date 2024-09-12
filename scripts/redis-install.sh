f0fd9dd45c7086188f35e1cad96055fb4c9e84bb
Majid <mj_azani@hotmail.com>
2024-09-12 20:30:23 +0930

    fixing redis install

--- scripts/redis-install.sh
+++ scripts/redis-install.sh
@@ -9,8 +9,8 @@ sudo sed -i 's/supervised no/supervised systemd/' /etc/redis/redis.conf
 sudo sed -i 's/bind 127.0.0.1 ::1/bind 127.0.0.1/' /etc/redis/redis.conf
 
 # Start and enable Redis service
-sudo systemctl start redis
-sudo systemctl enable redis
+sudo systemctl restart redis-server
+sudo systemctl enable redis-server
 
 # Verify Redis installation
 sudo redis-cli ping
